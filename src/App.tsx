df18f1ab42e599a3a2f6e9a8278bac0fb8497e45
Skanda J <skandaj813@gmail.com>
2024-05-24 23:01:26 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from 'react';
 import './App.css'
 import Block from './components/Block';
 
+type State = (string | null);
+
 function App() {
   const [state, setState] = useState(Array(9).fill(null));
   const [currentTurn, setCurrentTurn] = useState("X");
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState<string | null>(null);
   const checkWinner = (state: any[]) => {
     const win = [
       [0, 1, 2],
@@ -25,6 +27,10 @@ function App() {
       return null;
   }
   
+  const checkDraw = (state: any[]):boolean => {
+    return state.every(position => position!==null);
+  }
+
   const handleClick = (index: number) => {
     const stateCopy = Array.from(state);  
     if(stateCopy[index]!==null || winner) return;
@@ -34,20 +40,16 @@ function App() {
     setState(stateCopy);
   }
 
-
   useEffect(() =>{
     const win = checkWinner(state)
 
   if(win){
     setWinner(win);
+  } else if(checkDraw(state)){
+    setWinner('Draw');
   }
   }, [state])
-
-  useEffect(() =>{
-    if(winner){
-      alert('Game Over');
-    }
-  }, [winner])
+  
   const restart = () => {
     setState(Array(9).fill(null));
     setCurrentTurn('X');
@@ -72,7 +74,7 @@ function App() {
       <Block onClick={() => handleClick(7)} value={state[7]}/>
       <Block onClick={() => handleClick(8)} value={state[8]}/>
     </div>
-    {winner &&<h2>{`${winner} wins`}</h2>}
+    {winner &&<h2>{winner == "Draw"? "Its a draw":`${winner} wins`}</h2>}
     <button onClick={restart}>Play Again</button>
   </div>
 }
